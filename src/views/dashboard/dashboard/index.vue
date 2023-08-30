4bdaae6b3c00592e8bd5a14a035bfa4e69b77176
LahiruPrasad12 <lahirupr471@gmail.com>
2023-08-31 01:41:35 +0530

    created plant lists

--- src/views/dashboard/dashboard/index.vue
+++ src/views/dashboard/dashboard/index.vue
@@ -15,95 +15,24 @@
           <ion-grid>
 
             <div class="small-header anim" style="--delay: .3s; margin-left: 5px"> <ion-text color="#000000">
-              <h3>Overview</h3>
+              <h3>Images</h3>
             </ion-text></div>
             <ion-row>
               <ion-col size="12">
-                <div class="main-blogs">
-                  <div class="main-blog anim" style="--delay: .1s">
-                    <div class="main-blog__title">TOTAL STAFF</div>
-                    <div class="main-blog__title2">22</div>
-                    <div class="main-blog__author">
-                      <div class="author-detail">
-                        <div class="author-name">Staff Management</div>
-                        <div class="author-info">Live Count</div>
-                      </div>
-                    </div>
+                <div class="plant-card anim" style="--delay: .1s">
+                  <div class="image-container">
+                    <img :src="getImagePath('images.jpg')" alt="Plant 1">
+                  </div>
+                  <div class="details">
+                    <h2>Apple</h2>
+                    <p>Age: 10</p>
+<!--                    <p>Type: Apple</p>-->
+                  </div>
+                </div>
+              </ion-col>
+            </ion-row>
+          </ion-grid>
 
-                  </div>
-                  <div class="main-blog anim" style="--delay: .2s">
-                    <div class="main-blog__title">TOTAL ITEMS</div>
-                    <div class="main-blog__title2">54</div>
-                    <div class="main-blog__author tips">
-                      <div class="author-detail">
-                        <div class="author-name">Stock Management</div>
-                        <div class="author-info">Live Count</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </ion-col>
-            </ion-row>
-          </ion-grid>
-          <ion-grid>
-            <div class="small-header anim" style="--delay: .3s; margin-left: 5px">Analytics</div>
-            <ion-row>
-              <ion-col size="4">
-                <div class="video anim" style="--delay: .5s">
-                  <div class="video-wrapper">
-                    <img src="https://i.postimg.cc/Qtk5bhM0/staff-c.jpg" type="">
-                    <div class="author-img__wrapper video-author">
-                      <svg class="feather feather-check" fill="none" stroke="currentColor" stroke-linecap="round"
-                           stroke-linejoin="round"
-                           stroke-width="3" viewBox="0 0 24 24">
-                        <path d="M20 6L9 17l-5-5"/>
-                      </svg>
-                      <img class="author-img" src="https://i.postimg.cc/0Nv7Jzkb/staff2.jpg"/>
-                    </div>
-                  </div>
-                  <div class="video-by">Users</div>
-                  <!--                <div class="video-name">TOTAL STAFF MEMBERS</div>-->
-                  <div class="video-name2">10</div>
-                </div>
-              </ion-col>
-              <ion-col size="4">
-                <div class="video anim" style="--delay: .6s">
-                  <div class="video-wrapper">
-                    <img src="https://i.postimg.cc/RV3hCvzy/item.jpg" type="">
-                    <div class="author-img__wrapper video-author">
-                      <svg class="feather feather-check" fill="none" stroke="currentColor" stroke-linecap="round"
-                           stroke-linejoin="round"
-                           stroke-width="3" viewBox="0 0 24 24">
-                        <path d="M20 6L9 17l-5-5"/>
-                      </svg>
-                      <img class="author-img" src="https://i.postimg.cc/tRcPNSR0/item2.jpg"/>
-                    </div>
-                  </div>
-                  <div class="video-by">Staff</div>
-                  <!--                <div class="video-name">TOTAL STOCK MANAGERS</div>-->
-                  <div class="video-name2">5</div>
-                </div>
-              </ion-col>
-              <ion-col size="4">
-                <div class="video anim" style="--delay: .4s">
-                  <div class="video-wrapper">
-                    <img src="https://i.postimg.cc/50HTYKN4/sup.jpg" type="">
-                    <div class="author-img__wrapper video-author">
-                      <svg class="feather feather-check" fill="none" stroke="currentColor" stroke-linecap="round"
-                           stroke-linejoin="round"
-                           stroke-width="3" viewBox="0 0 24 24">
-                        <path d="M20 6L9 17l-5-5"/>
-                      </svg>
-                      <img class="author-img" src="https://i.postimg.cc/cHX7WdN6/sup2.jpg"/>
-                    </div>
-                  </div>
-                  <div class="video-by">Suppliers</div>
-                  <!--                <div class="video-name">TOTAL SHIPPING ITEMS</div>-->
-                  <div class="video-name2">2</div>
-                </div>
-              </ion-col>
-            </ion-row>
-          </ion-grid>
         </ion-list>
       </div>
     </ion-content>
@@ -176,10 +105,48 @@ export default {
     IonText,
     IonRippleEffect
   },
-  name: "index"
+  name: "index",
+
+  methods: {
+    getImagePath(imageFileName) {
+      return require(`@/assets/${imageFileName}`);
+    }
+  }
 }
 </script>
 
 <style scoped>
+.plant-card {
+  display: flex;
+  border: 1px solid #ccc;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
 
+.image-container {
+  flex: 1;
+  margin-right: 10px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 4px;
+}
+
+.details {
+  flex: 0.5;
+  margin-top: 5px;
+}
+
+h2 {
+  font-size: 1.2rem;
+  margin: 0;
+}
+
+p {
+  margin: 5px 0;
+}
 </style>
