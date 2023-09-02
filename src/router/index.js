e3bc3e7a602589809facb2c1973a4d6147afe0c0
LahiruPrasad12 <lahirupr471@gmail.com>
2023-09-02 11:06:51 +0530

    delete unusable files

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import Auth from './modules/auth'
-import SupplierTab from '../views/supplier/index'
-import StockTab from '../views/stock/index'
 import TabsPage from '../views/dashboard/index'
-import OrdersPage from '../views/orders/index'
 
 const routes = [
     ...Auth,
@@ -33,52 +30,6 @@ const routes = [
             },
         ]
     },
-    {
-        path: '/supplier/',
-        component: SupplierTab,
-        children: [
-            {
-                path: 'home',
-                component: () => import('@/views/supplier/items/index')
-            },
-            {
-                path: 'request',
-                component: () => import('@/views/supplier/request/index')
-            }
-        ]
-    },
-    {
-        path: '/stock/',
-        component: StockTab,
-        children: [
-            {
-                path: 'home',
-                component: () => import('@/views/stock/stocks/index')
-            },
-            {
-                path: 'purchaseorder',
-                component: () => import('@/views/supplier/request/index')
-            },
-            {
-                path: 'supplyrequest',
-                component: () => import('@/views/stock/request/index')
-            }
-        ]
-    },
-    {
-        path: '/orders/',
-        component: OrdersPage,
-        children: [
-            {
-                path: 'home',
-                component: () => import('@/views/orders/orders/index')
-            },
-            {
-                path: 'canceledOrders',
-                component: () => import('@/views/orders/canceledOrders/index')
-            }
-        ]
-    }
 ]
 
 const router = createRouter({
